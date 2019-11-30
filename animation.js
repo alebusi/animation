981da86f0020f1e07ea8dca9b1610adb6e8245f6
alebusi <allebusi@gmail.com>
2019-11-30 11:39:10 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,8 +1,7 @@
 var stati = ["zero","uno","due","tre","quattro"];
 var ind = 0;
 
-function switch() {
-  var className = div.getAttribute("class");
+function cambio() {
   ind+=1;
   if (ind > stati.length-1) ind=0;
   document.getElementById("elemento").className=stati[ind]);
