8273650fdd60056b37f22cc06e0f86c27bdba3b6
alebusi <allebusi@gmail.com>
2019-12-03 12:14:02 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -5,4 +5,12 @@ function cambio() {
   ind+=1;
   if (ind > stati.length-1) ind=0;
   document.getElementById("elemento").className=stati[ind];
+  if (document.getElementById("elemento").className=="quattro")
+     {
+     document.getElementById("elemento2").className="acceso");
+     }
+  else
+     {
+     document.getElementById("elemento2").className="spento");
+     }
 }
