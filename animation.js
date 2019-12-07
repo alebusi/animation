d6aff0ca2fe27d2103bf391c590c736983ae7d97
alebusi <allebusi@gmail.com>
2019-12-07 19:32:59 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,20 +1,14 @@
-var stati = ["zero","uno","due","tre","quattro"];
+var animazioni = ["orizzontale","orizzontale","verticale","verticale","bersaglio"];
 var colori = ["orange","#ff3300","#A1E2FF","#FF7FFF","#00C200","white"];
+var barra = ["orizzontale_bv","orizzontale_bo","verticale_bv","verticale_bo","bersaglio_zoom"];
 var ind = 0;
 var ind_c = 0;
 
 function cambioAnimazione() {
   ind+=1;
-  if (ind > stati.length-1) ind=0;
-  document.getElementById("elemento").className=stati[ind];
-  if (document.getElementById("elemento").className=="quattro")
-     {
-     document.getElementById("elemento2").className="acceso";
-     }
-  else
-     {
-     document.getElementById("elemento2").className="spento";
-     }
+  if (ind > animazioni.length-1) ind=0;
+  document.getElementById("elemento").className=animazioni[ind];
+  document.getElementById("elemento2").className=barra[ind];
 }
 
 function cambioColore() {
