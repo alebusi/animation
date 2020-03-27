597d98c960a58eea015b03f2541b2c768bc7873c
alebusi <allebusi@gmail.com>
2020-03-27 12:53:21 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,9 +1,9 @@
-var animazioni = ["orizzontale","orizzontale","orizzontale","orizzontale","verticale","verticale","verticale","verticale","bersaglio","rettangolo","rettangolo","pianeta"];
-var colori = ["orange","#ff3300","#00FFFF","#FF00FF","#00C200","black","white"];
-var barra = ["orizzontale_bv","orizzontale_bo","verticale_bv","rettangolo_b","verticale_bo","verticale_bv","orizzontale_bo","rettangolo_b","bersaglio_zoom","rettangolo_b","rettangolo_dot","satellite"];
-var ind = 0;
-var ind_c = 0;
-var valoreOpacity = 0;
+var animazioni = ["orizzontale","verticale","rettangolo","bersaglio"];
+var colori = ["white","orange","#ff3300","#00FFFF","#FF00FF","#00C200","black"];
+var barra = ["vuoto","orizzontale_b","verticale_b","rettangolo_b","rettangolo_dot","bersaglio_zoom","satellite"];
+var ind_anim = 0;
+var ind_guida = 0;
+var ind_col = 0;
 
 function verTastoPremuto(e) {
   if (e.keyCode == 32) {
@@ -18,42 +18,48 @@ function verTastoPremuto(e) {
 }
 		 
 function cambioAnimazione() {
-  ind+=1;
-  if (ind > animazioni.length-1) ind=0;
-  document.getElementById("elemento").className=animazioni[ind];
-  document.getElementById("elemento2").className=barra[ind];
-  document.getElementById("elemento2").style.borderRadius="0px";
+  ind_anim+=1;
+  if (ind_anim > animazioni.length-1) ind_anim=0;
+  document.getElementById("elemento").className=animazioni[ind_anim];
+}
+
+function cambioGuida() {
+  ind_guida+=1;
+  if (ind_guida > barra.length-1) ind_guida=0;
+  document.getElementById("elemento2").className=barra[ind_guida];
 }
 
 function cambioColore() {
-  ind_c+=1;
-  if (ind_c > colori.length-1) ind_c=0;
-  document.getElementById("elemento").style.backgroundColor = colori[ind_c];
+  ind_col+=1;
+  if (ind_col > colori.length-1) ind_col=0;
+  document.getElementById("elemento").style.backgroundColor = colori[ind_col];
+  document.getElementById("elemento").style.borderWidth="0px";
 }
 
 function cambioForma() {
   if (document.getElementById("elemento").style.borderRadius == "0px") {
-	 document.getElementById("elemento").style.borderRadius = "100px";
+	   document.getElementById("elemento").style.borderRadius = "100px";
   }
   else {
-         document.getElementById("elemento").style.borderRadius = "0px";
+     document.getElementById("elemento").style.borderRadius = "0px";
   }
   if (document.getElementById("elemento").className == "bersaglio") {
       if (document.getElementById("elemento").style.borderRadius == "0px") {
-	 document.getElementById("elemento2").style.borderRadius = "0px"; 
+	       document.getElementById("elemento2").style.borderRadius = "0px"; 
       }
       else {
-	 document.getElementById("elemento2").style.borderRadius = "70vh";
+	       document.getElementById("elemento2").style.borderRadius = "70vh";
       }
   }  
 }
 
-function cambioOpac() {
-  if (valoreOpacity == 0) {
-    valoreOpacity = 1;
+function cambioStile() {
+  if (document.getElementById("elemento").style.backgroundColor != "transparent") {
+    document.getElementById("elemento").style.backgroundColor="transparent";
+    document.getElementById("elemento").style.border="solid 12px "+colori[ind_col];
   }
   else {
-    valoreOpacity = 0;  
+    document.getElementById("elemento").style.backgroundColor=colori[ind_col];
+    document.getElementById("elemento").style.borderWidth="0px";
   }
-  document.getElementById("elemento2").style.opacity = valoreOpacity;
 }
